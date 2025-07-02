3f317553e9a1a2e66a5de2d99af760b1b33bfe26
singhalt422 <singhalt422@gmail.com>
2025-07-02 21:32:09 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
